74b968db7f08218b304c3ecb736ad440595a5aee
monicamoni <monica@chimple.org>
2016-01-15 11:40:11 +0530

    Initial commit

---
+++ Dictionary/ZIPPY/js/boot1.js
@@ -0,0 +1,14 @@
+var boot1State = {
+
+	preload: function () {
+ game.load.image('progressBar', 'assets/progressBar.png');
+	},
+
+	create: function() { 
+        //game.stage.backgroundColor = '#3498db';
+		//me.sprite = game.add.sprite(0, 0, 'background');
+		//game.physics.startSystem(Phaser.Physics.ARCADE);
+
+		game.state.start('load1');
+	}
+};
