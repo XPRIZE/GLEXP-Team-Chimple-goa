af9dd9522801c1ae647a1290b75ead16cf9dd729
kirankumarcs <kiran@chimple.org>
2016-03-27 16:44:03 +0530

    Isometric

--- explore/src/navigate/index.js
+++ explore/src/navigate/index.js
@@ -1,26 +1,27 @@
-/// <reference path="../node_modules/phaser/typescript/phaser.d.ts" />
+import StateHolder from '../StateHolder.js';
+import BootState from './states/Bootstate';
+import ExampleState from './states/ExampleState';
+import City1State from './states/City1State';
+import City2State from './states/City2State';
+import GameState from '../scene/states/GameState';
 
-import BootState from 'states/Bootstate';
-import ExampleState from 'states/ExampleState';
-import City1State from 'states/City1State';
-import City2State from 'states/City2State';
-import GameState from 'states/GameState';
+export default class NavigateStateHolder extends StateHolder {
 
-class Game extends Phaser.Game {
-
-	constructor() {
-		super(1280, 800, Phaser.AUTO, 'content', null);
-      
-       
-        this.state.add('bootState', BootState, false);
-        this.state.add('exampleState', ExampleState, false);
-        this.state.add('city1State', City1State, false);
-        this.state.add('city2State', City2State, false);
-        this.state.add('gameState', GameState, false);
-		this.state.start('bootState');
+	constructor(game) {
+		super(game);
+    }
+    
+    createStates () {   
+        this.game.state.add('bootState', BootState, false);
+        this.game.state.add('exampleState', ExampleState, false);
+        this.game.state.add('city1State', City1State, false);
+        this.game.state.add('city2State', City2State, false);
+        this.game.state.add('gameState', GameState, false);
+		
 	}
+    startDefault() {
+        this.game.state.start('bootState');
+    }
 
     
 }
-
-window.game = new Game();
