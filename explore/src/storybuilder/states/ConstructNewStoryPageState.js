82c853b081402e56ce39176bdf0eef7ef06072c9
Pankaj <pankaj@chimple.org>
2016-03-28 16:20:20 +0530

    changes in UI

--- explore/src/storybuilder/states/ConstructNewStoryPageState.js
+++ explore/src/storybuilder/states/ConstructNewStoryPageState.js
@@ -127,6 +127,11 @@ export default class ConstructNewStoryPageState extends Phaser.State {
 
         this._currentStory = this.loadStoryFromLocalStorage(this._currentStoryId);
         this._currentPage = this.loadStoryPageToEdit();
+        
+        if(this._currentPage.questionsAndAnswers==undefined)
+        {
+            this._currentPage.questionsAndAnswers = [];
+        }
     }
 
     loadScenesConfiguration() {
@@ -478,6 +483,7 @@ export default class ConstructNewStoryPageState extends Phaser.State {
 
     // after recording play end will show next button to ask the questions
     nextButton() {
+        this._nextButton.alpha = 0;
         console.log("next button");
         window.display_question_multichoice();
     }
