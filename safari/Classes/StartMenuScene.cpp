0caeada9461db42ab2dd6412352123fc71331918
Srikanth Talapadi <talapadi@users.noreply.github.com>
2016-07-19 15:41:54 +0530

    Added independent handling of creating scene

--- safari/Classes/StartMenuScene.cpp
+++ safari/Classes/StartMenuScene.cpp
@@ -9,6 +9,7 @@
 #include "StartMenuScene.h"
 #include "HelloWorldScene.h"
 #include "alphamon/SelectAlphamonScene.h"
+#include "puzzle/DuelScene.h"
 #include "mini_games/PatchTheWallScene.h"
 #include "mini_games/CrossTheBridgeScene.h"
 #include "mini_games/SmashTheRockLevelScene.h"
@@ -16,6 +17,9 @@
 #include "mini_games/TraceScene.h"
 
 USING_NS_CC;
+
+static const std::string DUEL_SCENE = "DuelScene";
+static const std::string ALPHAMON_COMBAT = "AlphamonCombat";
 
 StartMenu::StartMenu(){
     
@@ -40,9 +44,9 @@ bool StartMenu::init() {
                                            [&](Ref *sender) {
                                                Director::getInstance()->replaceScene(HelloWorld::createScene("camp","",""));
                                            });
-    auto menuItem2 = MenuItemLabel::create(Label::createWithTTF("Alphamon", "fonts/arial.ttf", 100),
+    auto menuItem2 = MenuItemLabel::create(Label::createWithTTF(ALPHAMON_COMBAT, "fonts/arial.ttf", 100),
                                            [&](Ref *sender) {
-                                               Director::getInstance()->replaceScene(SelectAlphamon::createScene());
+                                               startScene(ALPHAMON_COMBAT);
                                            });
 	auto menuItem3 = MenuItemLabel::create(Label::createWithTTF("Patch The Wall", "fonts/arial.ttf", 100),
 		[&](Ref *sender) {
@@ -70,4 +74,12 @@ bool StartMenu::init() {
     addChild(menu);
     
     return true;
+}
+
+void StartMenu::startScene(std::string gameName, std::string firstParam, std::string secondParam, std::string thirdParam) {
+    if(gameName == ALPHAMON_COMBAT) {
+        Director::getInstance()->replaceScene(SelectAlphamon::createScene());
+    } else if(gameName == DUEL_SCENE) {
+        Director::getInstance()->replaceScene(DuelScene::createScene(firstParam.at(0), secondParam.at(0)));
+    }
 }
