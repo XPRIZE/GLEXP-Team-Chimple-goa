9f3278537b485a885061d7b0ef0727ce3a03a995
Srikanth Talapadi <talapadi@users.noreply.github.com>
2016-07-23 23:49:36 +0530

    added multi language support for alphabets

--- safari/proj.android/jni/Android.mk
+++ safari/proj.android/jni/Android.mk
@@ -52,6 +52,9 @@ LOCAL_SRC_FILES := hellocpp/main.cpp \
 					../../Classes/mini_games/Cannon_Ball_Listener.cpp \
 					../../Classes/mini_games/Cannon_Ball_Main.cpp \
 					../../Classes/menu/MenuContext.cpp \
+					../../Classes/lang/LangUtil.cpp \
+					../../Classes/lang/EnglishUtil.cpp \
+					../../Classes/lang/KannadaUtil.cpp \
                     ../../Classes/StartMenuScene.cpp \
                 	../../Classes/AlphamonSprite.cpp \
                 	../../Classes/SkeletonPosition.cpp
