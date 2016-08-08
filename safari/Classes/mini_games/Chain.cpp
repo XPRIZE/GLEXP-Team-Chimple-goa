7726db082881810c7f45335fccab5d73df90579e
bhaveshh <bhavesh@chimple.org>
2016-08-08 19:09:33 +0530

    ChainGame start with new framework

--- safari/Classes/mini_games/Chain.cpp
+++ safari/Classes/mini_games/Chain.cpp
@@ -4,39 +4,64 @@
 #include "../alphamon/Alphamon.h"
 #include "../puzzle/CharGenerator.h"
 #include "../lang/LangUtil.h"
+#include "editor-support/cocostudio/CocoStudio.h"
 
 USING_NS_CC;
 
 Scene* Chain::createScene()
 {
-	// 'scene' is an autorelease object
-	auto scene = Scene::create();
-
 	// 'layer' is an autorelease object
 	auto layer = Chain::create();
 
-	// add layer as a child to scene
-	scene->addChild(layer);
-	/*layer->_menuContext = MenuContext::create(layer, Chain::gameName());
-	scene->addChild(layer->_menuContext);*/
+	// 'scene' is an autorelease object
+	auto scene = GameScene::createWithChild(layer, "chain");
 
+	layer->_menuContext = scene->getMenuContext();
 	return scene;
 }
-
-// on 'init' you need to initialize your instance
-bool Chain::init()
-{
-	//SpriteFrameCache::getInstance()->addSpriteFramesWithFile("crossthebridge.plist");
-
-	// 1. super init first
-	if (!Layer::init())
-	{
-		return false;
-	}
-
-	auto chain = CSLoader::createNode("chain/chain.csb");
-	this->addChild(chain, 0);
-	//chain->setAnchorPoint(Vec2(0.5,0.5));
-
-	return true;
+Node* Chain::loadNode() {
+	auto node = CSLoader::createNode("chain/chain.csb");
+	return node;
 }
+void Chain::createChoice() {
+	_choice = Node::create();
+	_choice->setPosition(Vec2(1280, 900));
+	addChild(_choice);
+	for (int i = 0; i < _numGraphemes; i++) {
+		auto choiceNode = Node::create();
+		choiceNode->setPosition(Vec2(i * 200, 0));
+		addChoice(choiceNode);
+	}
+}
+std::string Chain::getGridBackground() {
+	return "chain/letterpanel.png";
+}
+Chain* Chain::create() {
+	Chain* word = new (std::nothrow) Chain();
+	if (word && word->init())
+	{
+		word->autorelease();
+		return word;
+	}
+	CC_SAFE_DELETE(word);
+	return nullptr;
+}
+
+
+//// on 'init' you need to initialize your instance
+//bool Chain::init()
+//{
+//	//SpriteFrameCache::getInstance()->addSpriteFramesWithFile("crossthebridge.plist");
+//
+//	// 1. super init first
+//	if (!Layer::init())
+//	{
+//		return false;
+//	}
+//
+//	auto chain = CSLoader::createNode("chain/chain.csb");
+//	this->addChild(chain, 0);
+//	//chain->setAnchorPoint(Vec2(0.5,0.5));
+//
+//	return true;
+//}
