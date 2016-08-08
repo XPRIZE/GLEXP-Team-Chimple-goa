2e64d3ebd3df8b8b9544f8bd95aa50eb0195ac72
monicamoni <monica@chimple.org>
2016-08-08 16:04:27 +0530

    jazz

--- safari/Classes/GameMapScene.cpp
+++ safari/Classes/GameMapScene.cpp
@@ -16,6 +16,7 @@
 #include "mini_games/EndlessRunner.h"
 #include "mini_games/Cannon_Ball_Main.h"
 #include "mini_games/Jasmin_Mainfile.h"
+#include "mini_games/jazz.h"
 #include "mini_games/TraceScene.h"
 #include "mini_games/AlphamonFeedScene.h"
 #include "StartMenuScene.h"
@@ -155,7 +156,7 @@ void GameMapScene::islandSelected(Ref* pSender, ui::Widget::TouchEventType eEven
             } else if(clickedButton->getName() == PATCH_THE_WALL) {
                 Director::getInstance()->replaceScene(PatchTheWall::createScene());
             } else if(clickedButton->getName() == SMASH_THE_ROCK) {
-                Director::getInstance()->replaceScene(SmashTheRock::createScene());
+                Director::getInstance()->replaceScene(jazz::createScene());
             } else if(clickedButton->getName() == CANNON_BALL) {
                 Director::getInstance()->replaceScene(Jasmin_Mainfile::createScene());
             } else if(clickedButton->getName() == ENDLESS_RUNNER) {
