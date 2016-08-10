ab676a7fb8bbe48048b72b912fbb28e4e380727a
Pankaj <pankaj@chimple.org>
2016-08-10 22:27:29 +0530

    change in csb

--- safari/Classes/mini_games/Jasmin_Mainfile.h
+++ safari/Classes/mini_games/Jasmin_Mainfile.h
@@ -11,12 +11,18 @@ class Jasmin_Mainfile : public WordScene
 public:
 	static cocos2d::Scene* createScene(); 
 	static Jasmin_Mainfile *create();
-
+	void startFire(Node *, int);
+	
 protected:
 	cocos2d::Node* loadNode() override;
 	std::string getGridBackground() override;
 	void createChoice() override;
 	void createAnswer() override;
+	void gameOver(bool correct);
+	std::vector<Node*> _treeArray;
+	std::vector<cocostudio::timeline::ActionTimeline*> _animationTimeline;
+	std::vector<cocos2d::Rect> _fileSequence;
+	Node* _tree;
 };
 
 #endif // __Jasmin_Mainfile_SCENE_H__
