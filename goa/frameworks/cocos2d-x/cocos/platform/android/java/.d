98c7be141e87dbe50207975860e13c5b1f303899
karim mirazul <karim@chimple.org>
2016-09-19 13:49:22 +0530

    NewGame_ShootingWord_added

---
+++ goa/frameworks/cocos2d-x/cocos/platform/android/java/.d
@@ -0,0 +1,4 @@
+D:\GitHub\GLEXP-Team-Chimple\goa\frameworks\cocos2d-x\cocos\platform\android\java\gen\com\enhance\gameservice\IGameTuningService.java : \
+  D:\GitHub\GLEXP-Team-Chimple\goa\frameworks\cocos2d-x\cocos\platform\android\java\src\com\enhance\gameservice\IGameTuningService.aidl
+
+D:\GitHub\GLEXP-Team-Chimple\goa\frameworks\cocos2d-x\cocos\platform\android\java\src\com\enhance\gameservice\IGameTuningService.aidl :
