504d728bb4da2321766696cd8715a7df9e82ba5f
shyamalu <shyamal@amiti.in>
2016-09-22 10:21:08 +0530

    re-generated JSB mapping for TextGenerator

--- goa/frameworks/cocos2d-x/cocos/scripting/js-bindings/auto/textgeneratorautobindings.hpp
+++ goa/frameworks/cocos2d-x/cocos/scripting/js-bindings/auto/textgeneratorautobindings.hpp
@@ -13,10 +13,13 @@ void js_textgeneratorautobindings_TextGenerator_finalize(JSContext *cx, JSObject
 void js_register_textgeneratorautobindings_TextGenerator(JSContext *cx, JS::HandleObject global);
 void register_all_textgeneratorautobindings(JSContext* cx, JS::HandleObject obj);
 bool js_textgeneratorautobindings_TextGenerator_getNumGraphemesInString(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_textgeneratorautobindings_TextGenerator_getSynonyms(JSContext *cx, uint32_t argc, jsval *vp);
 bool js_textgeneratorautobindings_TextGenerator_getAllChars(JSContext *cx, uint32_t argc, jsval *vp);
 bool js_textgeneratorautobindings_TextGenerator_generateMatrix(JSContext *cx, uint32_t argc, jsval *vp);
 bool js_textgeneratorautobindings_TextGenerator_generateASentence(JSContext *cx, uint32_t argc, jsval *vp);
 bool js_textgeneratorautobindings_TextGenerator_getValidCombinations(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_textgeneratorautobindings_TextGenerator_getAntonyms(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_textgeneratorautobindings_TextGenerator_getHomonyms(JSContext *cx, uint32_t argc, jsval *vp);
 bool js_textgeneratorautobindings_TextGenerator_getGraphemes(JSContext *cx, uint32_t argc, jsval *vp);
 bool js_textgeneratorautobindings_TextGenerator_generateAWord(JSContext *cx, uint32_t argc, jsval *vp);
 bool js_textgeneratorautobindings_TextGenerator_getInstance(JSContext *cx, uint32_t argc, jsval *vp);
