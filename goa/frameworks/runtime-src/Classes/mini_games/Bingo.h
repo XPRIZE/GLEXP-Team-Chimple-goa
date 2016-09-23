936a1af30219c9c03602d7f011ad97f245460c25
bhaveshh <bhavesh@chimple.org>
2016-09-23 16:39:29 +0530

    Bingo game

--- goa/frameworks/runtime-src/Classes/mini_games/Bingo.h
+++ goa/frameworks/runtime-src/Classes/mini_games/Bingo.h
@@ -6,6 +6,7 @@
 #include "../lang/TextGenerator.h"
 #include "../puzzle/WordScene.h"
 #include "../GameScene.h"
+#include "../effects/FShake.h"
 
 using namespace cocos2d;
 
@@ -15,9 +16,11 @@ public:
 	static cocos2d::Scene* createScene();
 	virtual bool init();
 	void addEvents(Sprite* touchSprite);
+	void setWordInHelpBoard();
 	std::vector<std::vector<Sprite *>> createGrid(int row , int column);
 	std::vector<std::vector<cocostudio::timeline::ActionTimeline *>> createGridOfCharcater(int row, int column);
 	void setAllSpriteProperties(Sprite* object, int zOrder, float posX, float posY, bool visibility, float anchorPointX, float anchorPointY, float rotation, float scaleX, float scaleY, Sprite* parent);
+	void setAllSpritePropertiesParentLabel(Sprite* object, int zOrder, float posX, float posY, bool visibility, float anchorPointX, float anchorPointY, float rotation, float scaleX, float scaleY, cocos2d::LabelTTF* parent);
 	 Bingo();
 	~Bingo();
 	int bingoHorizotally();
@@ -32,11 +35,18 @@ protected:
 
 	Size visibleSize = Director::getInstance()->getVisibleSize();
 	Vec2 origin = Director::getInstance()->getVisibleOrigin();
-	std::vector<cocos2d::Sprite*> _boxes;
+
 	std::vector<std::vector<Sprite *>> _boxContainer;
 	std::vector<std::vector<Sprite *>> _charFace;
 	std::vector<std::vector<cocostudio::timeline::ActionTimeline *>> _charAnimContainer;
+	std::vector<std::vector<Sprite *>> _bingoAnimBin;
 	Sprite* _boxBoard;
+	Sprite* _helpBoard;
+	std::map<std::string, std::string> _data;
+	std::vector<std::string> _data_key;
+	std::vector<std::string> _data_value;
+	cocos2d::LabelTTF* _label = NULL;
+	bool _isBingoDone = false;
 
 	// implement the "static create()" method manually
 	CREATE_FUNC(Bingo);
