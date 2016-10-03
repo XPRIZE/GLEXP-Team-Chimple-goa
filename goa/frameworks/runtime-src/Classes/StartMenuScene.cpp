1e0b8de363b742f5dabe40b7585e64e295fa65d3
monicamoni <monica@chimple.org>
2016-10-03 12:15:29 +0530

    circle(changes)

--- goa/frameworks/runtime-src/Classes/StartMenuScene.cpp
+++ goa/frameworks/runtime-src/Classes/StartMenuScene.cpp
@@ -30,6 +30,7 @@
 #include "mini_games/Memory.h"
 #include "mini_games/MemoryHero.h"
 #include "mini_games/MemoryJungle.h"
+#include "mini_games/Circle.h"
 
 
 USING_NS_CC;
@@ -136,7 +137,9 @@ void StartMenu::startScene(std::string gameName, std::string firstParam, std::st
 		ScriptingCore::getInstance()->runScript("src/start/jump.js");
 	} else if (gameName == POP) {
 		ScriptingCore::getInstance()->runScript("src/start/pop.js");
-	} else if (gameName == BUBBLE) {
+	}else if (gameName == CIRCLE) {
+		Director::getInstance()->replaceScene(Circle::createScene());
+	}else if (gameName == BUBBLE) {
 		ScriptingCore::getInstance()->runScript("src/start/BubbleShooter.js");
 	}else if (gameName == PINATA) {
 		ScriptingCore::getInstance()->runScript("src/start/pinata.js");
