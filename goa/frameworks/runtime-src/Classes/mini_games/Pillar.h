19e270c571957a820ee8948b0ab5a64250cee726
monicamoni <monica@chimple.org>
2016-10-17 09:46:16 +0530

    layer

--- goa/frameworks/runtime-src/Classes/mini_games/Pillar.h
+++ goa/frameworks/runtime-src/Classes/mini_games/Pillar.h
@@ -16,15 +16,23 @@ public:
 	static cocos2d::Scene* createScene();
 	std::map<std::string, std::string> _scenePath;
 	float extraX;
+	int _num = 0;
+	int _score = 0;
 	virtual bool onTouchBegan(cocos2d::Touch *touch, cocos2d::Event * event);
     cocos2d::Node* background;
 	cocos2d::Sprite* _cake;
 	cocos2d::Sprite* _cakeMove;
 	cocos2d::Node* _ladder;
+	cocos2d::Node* _character;
+	cocos2d::Label* _topLabel;
 	cocos2d::Sprite* _pointRef;
 	std::vector<cocos2d::Node *>  _Ref;
+	std::vector<std::string> _wordCorrect;
+	std::vector<std::string> _wordList;
 	void ladderMove();
 	void newCake();
+	void blink(std::string animationName, bool loop);
+	static const char* gameName() { return PILLAR.c_str(); }
 protected:
 	bool _rotateFlag = true;
 	MenuContext * menu;
