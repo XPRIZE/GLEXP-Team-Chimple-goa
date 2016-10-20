78c5bb4705ea0922848d0f2c16f984bb6ae90ecb
shyamalu <Shyamal.Upadhyaya@Shyamals-MacBook-Pro.local>
2016-10-20 12:01:07 +0530

    workaround for pthread on windows

--- goa/frameworks/runtime-src/Classes/AsyncTask.cpp
+++ goa/frameworks/runtime-src/Classes/AsyncTask.cpp
@@ -7,22 +7,35 @@
 //
 
 #include "AsyncTask.hpp"
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_WINRT || CC_TARGET_PLATFORM == CC_PLATFORM_WIN32)
+#else
 #include "pthread.h"
+#endif
 #include "stdlib.h"
 
 void* AsyncTask::_doInBackground(void* pTask)
 {
+    #if (CC_TARGET_PLATFORM == CC_PLATFORM_WINRT || CC_TARGET_PLATFORM == CC_PLATFORM_WIN32)
+    #else
     ((AsyncTask*)pTask)->doInBackground();
     pthread_join(*(pthread_t*)(((AsyncTask*)pTask)->m_threadhandle), NULL);
     ((AsyncTask*)pTask)->onPostExecute();
+    #endif    
 }
 
 bool AsyncTask::execute()
 {
-    m_threadhandle = malloc(sizeof(pthread_t));
-    onPreExecute();
-    pthread_create((pthread_t*) m_threadhandle, NULL, _doInBackground, (void*)this);
-    return true;
+    #if (CC_TARGET_PLATFORM == CC_PLATFORM_WINRT || CC_TARGET_PLATFORM == CC_PLATFORM_WIN32)
+        onPreExecute();
+        doInBackground();
+        onPostExecute();
+    #else
+        m_threadhandle = malloc(sizeof(pthread_t));
+        onPreExecute();
+        pthread_create((pthread_t*) m_threadhandle, NULL, _doInBackground, (void*)this);
+        return true;
+    #endif
+
 }
 
 
