d934adf270b86acd6a1e95db2c68aac8c2f8535f
jpsgit <jp@chimple.org>
2016-11-03 18:37:56 +0530

    Added treasure hunt game

--- goa/frameworks/runtime-src/Classes/StartMenuScene.h
+++ goa/frameworks/runtime-src/Classes/StartMenuScene.h
@@ -53,6 +53,7 @@ static const std::string ORDER = "order";
 static const std::string PILLAR = "pillar";
 static const std::string CARDRAW = "cardraw"; 
 static const std::string BLASTLETTER = "BlastLetter";
+static const std::string TREASUREHUNT = "TreasureHunt";
 
 class StartMenu {
 public:
