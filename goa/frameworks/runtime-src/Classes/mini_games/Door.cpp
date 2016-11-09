f6b005ece641557733b0e0e5e0b1f2d2b9f31a35
monicamoni <monica@chimple.org>
2016-11-09 10:19:33 +0530

    door(changes)

--- goa/frameworks/runtime-src/Classes/mini_games/Door.cpp
+++ goa/frameworks/runtime-src/Classes/mini_games/Door.cpp
@@ -6,6 +6,8 @@
 #include "Door.h"
 #include "../WordSceneLipiTKNode.h"
 #include "DoorNode.h"
+#include "../lang/TextGenerator.h"
+#include "../menu/HelpLayer.h"
 
 USING_NS_CC;
 
@@ -59,18 +61,6 @@ bool Door::init()
 		background->setPositionX((visibleSize.width - 2560) / 2);
 	}
 	this->addChild(background);
-
-	
-	
-	/*this->runAction(Sequence::create(CallFunc::create([=]() {
-		auto timeline = CSLoader::createTimeline("doors/box.csb");
-		box->runAction(timeline);
-		timeline->play("open", false);
-	}), CallFunc::create([=]() {
-		auto timeline1 = CSLoader::createTimeline("doors/box.csb");
-		box->runAction(timeline1);
-		timeline1->play("close", false);
-	}), NULL));*/
 	return true;
 }
 void Door::onEnterTransitionDidFinish()
@@ -79,52 +69,151 @@ void Door::onEnterTransitionDidFinish()
 	Size visibleSize = Director::getInstance()->getVisibleSize();
 	float box2X = visibleSize.width / 2;
 
-	float box1Y = visibleSize.height*0.94;
+	auto level = menu->getCurrentLevel();
+	auto text = TextGenerator::getInstance();
 
+	
+	if(level<26)
+	{ 
+	_alphabet = LangUtil::getInstance()->getAllCharacters()[menu->getCurrentLevel() - 1];
+	_randomWord.append(6, _alphabet.at(0));
+	}else if(level<36)
+	{
+		_randomWord = text->generateAWord(menu->getCurrentLevel());
+	}else if (level<46)
+	{
+		_randomWord = text->generateAWord(menu->getCurrentLevel());
+	}else if (level<56)
+	{
+		_randomWord = text->generateAWord(menu->getCurrentLevel());
+	}else if (level<66)
+	{
+		_randomWord = text->generateAWord(menu->getCurrentLevel());
+	}
+	
+	_wordLength = _randomWord.size();
+
+	float boxUpperY = visibleSize.height*0.965;
+	float boxLowerY = visibleSize.height*0.48;
+
+	if (_wordLength == 2)
+	{
+		auto boxInside1 = Sprite::createWithSpriteFrameName("doors/boxinside.png");
+		boxInside1->setPosition(Vec2(box2X - boxInside1->getContentSize().width / 1.5, boxUpperY));
+		this->addChild(boxInside1);
+		boxInside1->setAnchorPoint(Vec2(0.5, 1));
+		auto box1 = CSLoader::createNode("doors/box.csb");
+		box1->setPosition(Vec2(box2X - boxInside1->getContentSize().width / 1.5, boxUpperY));
+		this->addChild(box1);
+		_BoxRef.pushBack(box1);
+		auto timeline = CSLoader::createTimeline("doors/box.csb");
+		box1->runAction(timeline);
+		//timeline->play("open", false);
+
+		auto boxInside2 = Sprite::createWithSpriteFrameName("doors/boxinside.png");
+		boxInside2->setPosition(Vec2(box2X + boxInside1->getContentSize().width / 1.5, boxUpperY));
+		this->addChild(boxInside2);
+		boxInside2->setAnchorPoint(Vec2(0.5, 1));
+		auto box2 = CSLoader::createNode("doors/box.csb");
+		box2->setPosition(Vec2(box2X + boxInside1->getContentSize().width / 1.5, boxUpperY));
+		this->addChild(box2);
+		_BoxRef.pushBack(box2);
+	}
+
+	if(_wordLength==3 || _wordLength==4 || _wordLength==5 || _wordLength==6)
+	{ 
 	auto boxInside1 = Sprite::createWithSpriteFrameName("doors/boxinside.png");
-	boxInside1->setPosition(Vec2(box2X - visibleSize.width / 3.3, box1Y));
+	boxInside1->setPosition(Vec2(box2X - visibleSize.width / 3.3, boxUpperY));
 	this->addChild(boxInside1);
 	boxInside1->setAnchorPoint(Vec2(0.5, 1));
 	auto box1 = CSLoader::createNode("doors/box.csb");
-	box1->setPosition(Vec2(box2X - visibleSize.width/3.3, box1Y));
+	box1->setPosition(Vec2(box2X - visibleSize.width/3.3, boxUpperY));
 	this->addChild(box1);
 	_BoxRef.pushBack(box1);
-	auto timeline = CSLoader::createTimeline("doors/box.csb");
-	box1->runAction(timeline);
-	//timeline->play("open", false);
+	
 
 	auto boxInside2 = Sprite::createWithSpriteFrameName("doors/boxinside.png");
-	boxInside2->setPosition(Vec2(box2X, box1Y));
+	boxInside2->setPosition(Vec2(box2X, boxUpperY));
 	this->addChild(boxInside2);
 	boxInside2->setAnchorPoint(Vec2(0.5, 1));
 	auto box2 = CSLoader::createNode("doors/box.csb");
-	box2->setPosition(Vec2(box2X, box1Y));
+	box2->setPosition(Vec2(box2X, boxUpperY));
 	this->addChild(box2);
 	_BoxRef.pushBack(box2);
 
 	auto boxInside3 = Sprite::createWithSpriteFrameName("doors/boxinside.png");
-	boxInside3->setPosition(Vec2(box2X + visibleSize.width / 3.3, box1Y));
+	boxInside3->setPosition(Vec2(box2X + visibleSize.width / 3.3, boxUpperY));
 	this->addChild(boxInside3);
 	boxInside3->setAnchorPoint(Vec2(0.5, 1));
 	auto box3 = CSLoader::createNode("doors/box.csb");
-	box3->setPosition(Vec2(box2X + visibleSize.width /3.3, box1Y));
+	box3->setPosition(Vec2(box2X + visibleSize.width /3.3, boxUpperY));
 	this->addChild(box3);
 	_BoxRef.pushBack(box3);
+	}
 
-	auto box4 = CSLoader::createNode("doors/box.csb");
-	box4->setPosition(Vec2(500, 900));
-	this->addChild(box4);
-	_BoxRef.pushBack(box4);
+	if (_wordLength == 4)
+	{
+		auto boxInside4 = Sprite::createWithSpriteFrameName("doors/boxinside.png");
+		boxInside4->setPosition(Vec2(box2X , boxLowerY));
+		this->addChild(boxInside4);
+		boxInside4->setAnchorPoint(Vec2(0.5, 1));
+		auto box4 = CSLoader::createNode("doors/box.csb");
+		box4->setPosition(Vec2(box2X, boxLowerY));
+		this->addChild(box4);
+		_BoxRef.pushBack(box4);
+	}
+	else if (_wordLength == 5)
+	{
+		auto boxInside4 = Sprite::createWithSpriteFrameName("doors/boxinside.png");
+		boxInside4->setPosition(Vec2(box2X - boxInside4->getContentSize().width/1.5, boxLowerY));
+		this->addChild(boxInside4);
+		boxInside4->setAnchorPoint(Vec2(0.5, 1));
+		auto box4 = CSLoader::createNode("doors/box.csb");
+		box4->setPosition(Vec2(box2X - boxInside4->getContentSize().width/1.5, boxLowerY));
+		this->addChild(box4);
+		_BoxRef.pushBack(box4);
 
-	auto box5 = CSLoader::createNode("doors/box.csb");
-	box5->setPosition(Vec2(1300, 900));
-	this->addChild(box5);
-	_BoxRef.pushBack(box5);
+		auto boxInside5 = Sprite::createWithSpriteFrameName("doors/boxinside.png");
+		boxInside5->setPosition(Vec2(box2X + boxInside4->getContentSize().width/1.5, boxLowerY));
+		this->addChild(boxInside5);
+		boxInside5->setAnchorPoint(Vec2(0.5, 1));
+		auto box5 = CSLoader::createNode("doors/box.csb");
+		box5->setPosition(Vec2(box2X + boxInside4->getContentSize().width/1.5, boxLowerY));
+		this->addChild(box5);
+		_BoxRef.pushBack(box5);
+    }
+	else if (_wordLength == 6)
+	{
+		auto boxInside4 = Sprite::createWithSpriteFrameName("doors/boxinside.png");
+		boxInside4->setPosition(Vec2(box2X - visibleSize.width / 3.3, boxLowerY));
+		this->addChild(boxInside4);
+		boxInside4->setAnchorPoint(Vec2(0.5, 1));
+		auto box4 = CSLoader::createNode("doors/box.csb");
+		box4->setPosition(Vec2(box2X - visibleSize.width / 3.3, boxLowerY));
+		this->addChild(box4);
+		_BoxRef.pushBack(box4);
+
+		auto boxInside5 = Sprite::createWithSpriteFrameName("doors/boxinside.png");
+		boxInside5->setPosition(Vec2(box2X , boxLowerY));
+		this->addChild(boxInside5);
+		boxInside5->setAnchorPoint(Vec2(0.5, 1));
+		auto box5 = CSLoader::createNode("doors/box.csb");
+		box5->setPosition(Vec2(box2X, boxLowerY));
+		this->addChild(box5);
+		_BoxRef.pushBack(box5);
+
+		auto boxInside6 = Sprite::createWithSpriteFrameName("doors/boxinside.png");
+		boxInside6->setPosition(Vec2(box2X + visibleSize.width / 3.3, boxLowerY));
+		this->addChild(boxInside6);
+		boxInside6->setAnchorPoint(Vec2(0.5, 1));
+		auto box6 = CSLoader::createNode("doors/box.csb");
+		box6->setPosition(Vec2(box2X + visibleSize.width / 3.3, boxLowerY));
+		this->addChild(box6);
+		_BoxRef.pushBack(box6);
+	}
+	
 
 	auto box6 = CSLoader::createNode("doors/box.csb");
-	box6->setPosition(Vec2(2100, 900));
-	this->addChild(box6);
-	_BoxRef.pushBack(box6);
 	float boxWidth = box6->getChildByName("boxdoor_6")->getContentSize().width;
 	float boxHeight = box6->getChildByName("boxdoor_6")->getContentSize().height;
 
@@ -133,25 +222,108 @@ void Door::onEnterTransitionDidFinish()
 		float x = _BoxRef.at(i)->getPositionX();
 		float y = _BoxRef.at(i)->getPositionY() - boxHeight/2;
 		
-		auto doorNode = DoorNode::create(boxWidth, boxHeight,Vec2( x, y));
-		this->addChild(doorNode);
-
+		_doorNode = DoorNode::create(boxWidth, boxHeight,Vec2( x, y));
+		this->addChild(_doorNode);
+		_doorNode->setParent(this);
+		_doorNodeRef.pushBack(_doorNode);
+		_doorNode->writingEnable(false);
+	}
+	if (_score == 0)
+	{
+		_doorNodeRef.at(_score)->writingEnable(true);
 	}
 	
-	/*auto doorNode2 = DoorNode::create(500, 500, Vec2(1250, 900));
-	this->addChild(doorNode2);
-	auto doorNode3 = DoorNode::create(500, 500, Vec2(1250, 900));
-	this->addChild(doorNode3);
-	auto doorNode4 = DoorNode::create(500, 500, Vec2(1250, 900));
-	this->addChild(doorNode4);
-	auto doorNode5 = DoorNode::create(500, 500, Vec2(1250, 900));
-	this->addChild(doorNode5);
-	auto doorNode6 = DoorNode::create(500, 500, Vec2(1250, 900));
-	this->addChild(doorNode6);*/
+	for (int i = 0; i<_wordLength; i++)
+	{
+		float x = _BoxRef.at(i)->getPositionX();
+		float y = _BoxRef.at(i)->getPositionY() - boxHeight / 2;
 
-
-
-
+		_myWord = _randomWord.at(i);
+		auto myLabel = Label::createWithBMFont(LangUtil::getInstance()->getBMFontFileName(), _myWord);
+		myLabel->setPositionX(x);
+		myLabel->setPositionY(y);
+		myLabel->setScale(0.8);
+		this->addChild(myLabel);
+		auto fadeOut = FadeOut::create(3.0f);
+		myLabel->runAction(fadeOut);
+	}
+	if (menu->getCurrentLevel() == 1) {
+		gameHelpLayer();
+	}
 	
+}
+void Door::gameHelpLayer()
+{
+	Size visibleSize = Director::getInstance()->getVisibleSize();
+	auto helpLayer = HelpLayer::create(Rect(_BoxRef.at(0)->getPositionX(), _BoxRef.at(0)->getPositionY() - _BoxRef.at(0)->getChildByName("boxdoor_6")->getContentSize().height / 2, _BoxRef.at(0)->getChildByName("boxdoor_6")->getContentSize().width, _BoxRef.at(0)->getChildByName("boxdoor_6")->getContentSize().height), Rect(0, 0, 0, 0));
+	std::vector <Point> points;
+	
+	float boxWidth = _BoxRef.at(0)->getChildByName("boxdoor_6")->getContentSize().width/2;
+	float boxHeight = _BoxRef.at(0)->getChildByName("boxdoor_6")->getContentSize().height/2;
+	
+	float x = _BoxRef.at(0)->getPositionX();
+	float y = _BoxRef.at(0)->getPositionY() - boxHeight;
+	points.push_back(Vec2(x - boxWidth / 1.25, y - boxHeight*0.6));
+	points.push_back(Vec2(x, y + boxHeight*0.7));
+	points.push_back(Vec2(x + boxWidth / 1.25, y - boxHeight*0.6));
+	points.push_back(Vec2(x - boxWidth / 2, y - boxHeight*0.1));
+	points.push_back(Vec2(x + boxWidth / 2, y - boxHeight*0.1));
+	helpLayer->writing(points);
+	this->addChild(helpLayer);
+	helpLayer->setName("gameHelpLayer");
+}
+void Door::clearScreen(float dt)
+{
+	if (_score < _randomWord.size())
+	{
+		_doorNodeRef.at(_score)->clearDrawing(nullptr, cocos2d::ui::Widget::TouchEventType::ENDED);
+	}
+}
 
+void Door::showScore(float dt)
+{
+	menu->showScore();
+}
+
+void Door::characterRecognisation(std::vector<string> str)
+{
+	this->removeChildByName("gameHelpLayer");
+	char letter= _randomWord.at(_score);
+	string word(&letter, 1);
+	CCLOG("character = %s", str.at(0).c_str());
+	if (str.at(0).compare(word) == 0)
+	{
+		auto timeline = CSLoader::createTimeline("doors/box.csb");
+		_BoxRef.at(_score)->runAction(timeline);
+		timeline->play("open", false);
+
+		_animalRef = {"sheep","pig","cow"};
+		auto path = "doors/" + _animalRef.at(cocos2d::RandomHelper::random_int(0, 2)) + ".csb";
+		auto boxInside = Sprite::createWithSpriteFrameName("doors/boxinside.png");
+		auto animal = CSLoader::createNode(path);
+		animal->setPositionX(_BoxRef.at(_score)->getPositionX() - boxInside->getContentSize().width/8);
+		animal->setPositionY(_BoxRef.at(_score)->getPositionY() - boxInside->getContentSize().height/2);
+		this->addChild(animal);
+		auto timeline1 = CSLoader::createTimeline(path);
+		animal->runAction(timeline1);
+		timeline1->play("walk", true);
+		if (_score < _randomWord.size())
+		{
+			_doorNodeRef.at(_score)->writingEnable(false);
+		}
+		_score++;
+		if (_score < _randomWord.size())
+		{
+			_doorNodeRef.at(_score)->writingEnable(true);
+		}
+		if (_score == _randomWord.size())
+		{
+			this->scheduleOnce(schedule_selector(Door::showScore), 6);
+		}
+	}
+	else
+	{
+		this->unschedule(schedule_selector(Door::clearScreen));
+		this->scheduleOnce(schedule_selector(Door::clearScreen), 3);
+	}
 }
