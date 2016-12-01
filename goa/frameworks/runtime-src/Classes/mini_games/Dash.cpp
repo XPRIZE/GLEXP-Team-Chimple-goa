d7f870d77f89efd728190d20e327186fd5ff8720
kirankumarcs <kiran@chimple.org>
2016-12-01 14:33:16 +0530

    Dash

--- goa/frameworks/runtime-src/Classes/mini_games/Dash.cpp
+++ goa/frameworks/runtime-src/Classes/mini_games/Dash.cpp
@@ -115,7 +115,7 @@ bool Dash::init()
 	};
 	
 	std::vector<std::string> theme = { "city","candy","iceLand" };
-	_scenePath = differntSceneMapping.at(theme.at(0));//cocos2d::RandomHelper::random_int(0, 2)));
+	_scenePath = differntSceneMapping.at(theme.at(cocos2d::RandomHelper::random_int(0, 2)));
 
 	auto spritecache1 = SpriteFrameCache::getInstance();
 	spritecache1->addSpriteFramesWithFile(_scenePath.at("plist"));
