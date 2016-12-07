ca9f07fc87ce790a9285b68285ec276b91f47edb
kirankumarcs <kiran@chimple.org>
2016-12-07 16:45:55 +0530

    Decomon

--- goa/frameworks/runtime-src/Classes/mini_games/Decomon.cpp
+++ goa/frameworks/runtime-src/Classes/mini_games/Decomon.cpp
@@ -347,7 +347,7 @@ bool Decomon::onTouchBegan(cocos2d::Touch * touch, cocos2d::Event * event)
 			}
 			else {
 				//if target is not a paint bucket set Scale to 1 and also creat duplicate
-				if (target->getScale() > 0 &&(!_colorPicked)) {
+				if (target->getScaleX() > 0 &&(!_colorPicked)) {
 					target->setScale(1);
 				}
 				else {
