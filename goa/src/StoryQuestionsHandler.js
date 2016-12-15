5967a029b2f45f2bda5f313a90423d6fe3947665
shyamalu <Shyamal.Upadhyaya@Shyamals-MacBook-Pro.local>
2016-12-15 18:45:46 +0530

    enable skip button for each question

--- goa/src/StoryQuestionsHandler.js
+++ goa/src/StoryQuestionsHandler.js
@@ -165,6 +165,7 @@ xc.StoryQuestionHandlerLayer = cc.Layer.extend({
 
     nextQuestion: function () {
         this._currentQuestionIndex++;
+        this._item.setEnabled(true);
         if(this._currentQuestionIndex < this._questions.length) {
             var question = this._questions[this._currentQuestionIndex];
             cc.log('question["type"]:' + question["type"]);
