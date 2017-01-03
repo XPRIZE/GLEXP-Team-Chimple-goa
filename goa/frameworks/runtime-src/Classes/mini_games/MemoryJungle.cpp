28baecffe0068d0ef7e7fdf8d081119c74f68f08
Srikanth Talapadi <talapadi@users.noreply.github.com>
2017-01-03 15:28:35 +0530

    Removed the removing of event listeners in MenuContext and fixed Memory to not show its own pairs

--- goa/frameworks/runtime-src/Classes/mini_games/MemoryJungle.cpp
+++ goa/frameworks/runtime-src/Classes/mini_games/MemoryJungle.cpp
@@ -632,9 +632,8 @@ bool MemoryJungle::onTouchBegan(Touch* touch, Event* event) {
 								_menuContext->showAnswer("wordPairs", _hint);
 							});
 
-							auto completeSequence = Sequence::create(createMatchLayer, DelayTime::create(0), showScore, NULL);
 
-							this->runAction(completeSequence);
+							this->runAction(showScore);
 						}
 						});
                     
