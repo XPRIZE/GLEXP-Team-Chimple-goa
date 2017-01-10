6c11d8f5bcf66d2d2bb6355024fc040c6f478b91
karim mirazul <karim@chimple.org>
2017-01-11 01:47:44 +0530

    fixed more than 6 letter word issues in defuse th bomb game

--- goa/frameworks/runtime-src/Classes/mini_games/BlastLetter.h
+++ goa/frameworks/runtime-src/Classes/mini_games/BlastLetter.h
@@ -33,6 +33,7 @@ public:
 	virtual bool init();
 	static BlastLetter* create();
 	void checkAlphabets();
+	void checkWordLength();
 	bool checkRecognizeLetter(string letter);
 	string getConvertInUpperCase(string data);
 	void addEventsOnGrid(cocos2d::Sprite * callerObject);
