93dad64f4c6a135e69797d4436c2b590cf231f7e
shyamalu <Shyamal.Upadhyaya@Shyamals-MacBook-Pro.local>
2017-01-16 15:46:18 +0530

    RPG Changes

--- goa/frameworks/runtime-src/Classes/ChooseCharacter.hpp
+++ goa/frameworks/runtime-src/Classes/ChooseCharacter.hpp
@@ -26,7 +26,7 @@
 #define GIRL_BUTTON "girl_button"
 #define BOY_TEXT "boy_text"
 #define GIRL_TEXT "girl_text"
-#define CHOOSE_CHARACTER_TEXT "Choose Character"
+#define CHOOSE_CHARACTER_TEXT "choose Character"
 #define BOY_LABEL_TEXT "Boy"
 #define GIRL_LABEL_TEXT "Girl"
 
