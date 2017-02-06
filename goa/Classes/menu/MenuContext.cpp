e4d4f74da5f9c738f6be22a11cb5cbec2400c073
bhaveshh <bhavesh@chimple.org>
2017-02-06 12:26:53 +0530

    Pop game is converted in c++

--- goa/Classes/menu/MenuContext.cpp
+++ goa/Classes/menu/MenuContext.cpp
@@ -72,6 +72,7 @@
 #include "../mini_games/Balloon.h"
 #include "../mini_games/PopCount.h"
 #include "../mini_games/DinoGame.h"
+#include "../mini_games/Pop.h"
 #include "../util/CommonLabel.h"
 #include "Setting.h"
 #include "Award.h"
@@ -893,8 +894,6 @@ void MenuContext::launchGameFinally(std::string gameName) {
     		Director::getInstance()->replaceScene(CatGame::createScene());
         } else if (gameName == TRAIN) {
 //            ScriptingCore::getInstance()->runScript("src/start/startFromCpp.js");
-        } else if (gameName == POP) {
-//            ScriptingCore::getInstance()->runScript("src/start/startFromCpp.js");
         } else if (gameName == STORY_TELLING) {
 //            ScriptingCore::getInstance()->runScript("start/storytelling.js");
         } else if (gameName == ALPHAMOLE) {
@@ -906,7 +905,7 @@ void MenuContext::launchGameFinally(std::string gameName) {
     	} else if (gameName == JUMP_ON_WORDS) {
 //    		ScriptingCore::getInstance()->runScript("src/start/startFromCpp.js");
     	} else if (gameName == POP) {
-//    		ScriptingCore::getInstance()->runScript("src/start/startFromCpp.js");
+			Director::getInstance()->replaceScene(Pop::createScene());
     	}else if (gameName == CIRCLE) {
     		Director::getInstance()->replaceScene(Circle::createScene());
     	}else if (gameName == BUBBLE) {
