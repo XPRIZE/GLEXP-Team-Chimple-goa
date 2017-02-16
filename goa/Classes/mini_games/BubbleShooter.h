8766bb83e8cdbbe1976d7fe690eb4577c9224f11
KARIM MIRAZUL <karim@chimple.org>
2017-02-17 01:08:30 +0530

    fixed bubbleShooter Game issue

--- goa/Classes/mini_games/BubbleShooter.h
+++ goa/Classes/mini_games/BubbleShooter.h
@@ -10,6 +10,14 @@
 #include "../lang/TextGenerator.h"
 #include "../effects/FShake.h"
 #include "SimpleAudioEngine.h"
+#include "../util/CommonLabelTTF.h"
+
+using namespace std;
+using namespace cocos2d;
+USING_NS_CC;
+
+class TileData;
+class Player;
 
 class BubbleShooter : public cocos2d::Layer
 {
@@ -20,14 +28,11 @@ protected:
 	bool _flagSwitchTwoColor = true;
 	std::string _imageSprite[8];
 	std::vector< std::vector<Sprite*> > _bubbleName;
-	std::vector< std::vector<Sprite*> > _LetterName;
-
+	std::vector< std::vector<CommonLabelTTF*> > _LetterName;
+	Player* player;
+	bool _helpActive = false;
 	//INITVARIABLES ------
 
-	bool _mainPlayerBubbleDestroy = false;
-	//var bubbleSizeReference = new cc.Sprite(cc.spriteFrameCache.getSpriteFrame("bubble_shooter/red_ball.png"));
-	
-	// Game states
 	struct gamestates  { 
 		int init = 0;
 		int ready = 1;
@@ -38,7 +43,7 @@ protected:
 	}gamestates;
 
 	int gamestate = gamestates.init;
-
+	bool _mainPlayerBubbleDestroy = false;
 	int rowoffset = 0;
 
 	// Animation variablespre
@@ -47,11 +52,11 @@ protected:
 
 	bool finalFlag = false;
 	bool killBubble = false;
-
+	float angle = 0.0f;
 	// Level
 	struct level{
-		int x = 0; // X position
-		int y = Director::getInstance()->getVisibleSize().height * 0.103;          // Y position
+		float x = 0.0f; // X position
+		float y = Director::getInstance()->getVisibleSize().height * 0.103;          // Y position
 		int width = 0;       // Width, gets calculated
 		int height = 0;      // Height, gets calculated
 		int columns = 13;    // Number of tile columns
@@ -59,18 +64,18 @@ protected:
 		int tilewidth = 0;  // Visual width of a tile
 		int tileheight = 0; // Visual height of a tile
 		int rowheight = 0;  // Height of a row
-		int radius = 0;     // Bubble collision radius
+		float radius = 0;     // Bubble collision radius
 		std::vector< std::vector<TileData*> > tiles;// The two-dimensional tile array
 	}level;
 
 	int counterhits = 0;
 
 	// Array Of Letter
-	std::vector<int> letterSprite;
-	std::vector<std::string> _imageSprite;
+	std::vector<std::string> letterSprite;
+	//std::vector<std::string> _imageSprite;
 	int _bubblecolors = 0;
 	std::vector<TileData *> _cluster;
-	std::vector<TileData *> _floatingclusters;
+	std::vector<std::vector<TileData *>> _floatingclusters;
 
 public:
 	~BubbleShooter();
@@ -79,50 +84,88 @@ public:
 	void onEnterTransitionDidFinish();
 	static BubbleShooter* create();
 	void update(float) override;
-	std::vector<std::vector<Sprite*>> create2dVector(int row, int column);
-	std::initializer_list <int> neighborsoffsets(int oddEven);
-
+	std::vector<std::vector<Sprite*>> create2dVectorSprite(int column, int row);
+	std::vector<std::vector<CommonLabelTTF*>> create2dVectorLetter(int column, int row);
+	std::vector<std::vector<TileData*>> create2dVectorTiles(int column, int row);
+	std::vector<std::pair<int, int>> neighborsoffsets(int oddEven);
+	bool circleIntersection(float x1, float y1, float r1, float x2, float y2, float r2);
+	void setGameState(int newgamestate);
+	int getRandomInt(int min, int max);
+	void createLevel(int color, int repeat);
+	int getRandomArbitrary(int min, int max);
+	void playerDie(float tilex, float tiley, int type);
+	std::vector<std::vector<TileData *>> findFloatingClusters();
+	void nextBubble();
+	void DataCard(std::string gamestatus);
+	void stateRemoveCluster();
+	int getExistingColor();
+	std::vector<int> findColors();
+	void renderTiles();
+	void bgListner();
+	void renderPlayer();
+	void checkBubbleStatus();
+	bool checkGameOver();
+	void drawNextLetter(float x, float y, int index);
+	void drawBubble(float x, float y, int index);
+	void drawNextBubble(float x, float y, int index);
+	void gunMove(float x, float y);
+	void onMouseMove(float posx, float posy);
+	std::vector<TileData *> getNeighbors(TileData* tile);
+	std::string getConvertInLowerCase(string data);
+	float radToDeg(float angle);
+	float degToRad(float angle);
+	void shootBubble();
+	void stateShootBubble(float dt);
+	void snapBubble();
+	void resetRemoved();
+	void resetProcessed();
+	std::vector<TileData *> findCluster(float tx, float ty, bool matchtype, bool reset, bool skipremoved);
+	std::pair<float, float> getGridPosition(float x, float y);
+	Sprite* drawBubbleGroups(float x, float y, int index, int col, int row);
+	CommonLabelTTF* drawLetterGroups(float x, float y, int index, int col, int row);
+	std::pair<float, float> getTileCoordinate(int column, int row);
+	void setSpriteProperties(Sprite* ImageObject, float positionX, float positionY, float scaleX, float scaleY, float anchorX, float anchorY, float rotation, int zorder);
 	static const char* gameName() { return BUBBLE.c_str(); }
 };
 
 
 
-class player {
+class Player {
 
 public:
-	int x = 0;
-	int y = 0;
+	float x = 0.0f;
+	float y = 0.0f;
 	float angle = 0;
 	int tiletype = 0;
 
 	struct bubble {
-		int x = 0;
-		int y = 0;
+		float x = 0.0f;
+		float y = 0.0f;
 		float angle = 0.0f;
 		int speed = 2500;
-		int tiletype = 0;
+		int tiletype = -1;
 		bool visible = false;
 	}bubble;
 	
 	struct nextbubble {
-		int x = 0;
-		int y = 0;
-		int tiletype = 0;
+		float x = 0.0f;
+		float y = 0.0f;
+		int tiletype = -1;
 	}nextbubble;
 
 };
 
 class TileData {
 public:
-	int x = 0;
-	int y = 0;
+	float x = 0.0f;
+	float y = 0.0f;
 	int type = 0;
 	bool removed = false;
 	int shift = 0;
 	int velocity = 0;
 	int alpha = 1;
 	bool processed = false;
-	TileData(int x, int y, int type,int shift);
+	TileData(float x, float y, int type,int shift);
 };
 
 #endif // __BubbleShooter_SCENE_H__
