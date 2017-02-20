72def35da65583f6672c385c536b23e5c1738b0c
talapadi <sri@zoolore.org>
2017-02-20 14:01:28 +0530

    Update Android.mk

--- goa/proj.android/jni/Android.mk
+++ goa/proj.android/jni/Android.mk
@@ -171,7 +171,7 @@ LOCAL_SRC_FILES := hellocpp/main.cpp \
          ../../Classes/mini_games/SpriteCreate.cpp \
          ../../Classes/mini_games/Stack.cpp \
          ../../Classes/mini_games/Step.cpp \
-		 ../../Classes/mini_games/Shoot.cpp \
+         ../../Classes/mini_games/Shoot.cpp \
          ../../Classes/mini_games/Table.cpp \
          ../../Classes/mini_games/Talk.cpp \
          ../../Classes/mini_games/TraceScene.cpp \
@@ -195,7 +195,7 @@ LOCAL_SRC_FILES := hellocpp/main.cpp \
          ../../Classes/puzzle/WordScene.cpp \
          ../../Classes/splash/SplashScene.cpp \
          ../../Classes/sqlite3/Sqlite3Helper.cpp \
-		 ../../Classes/sqlite3/sqlite3.c \
+         ../../Classes/sqlite3/sqlite3.c \
          ../../Classes/story/CopyRight.cpp \
          ../../Classes/story/FillInTheBlanks.cpp \
          ../../Classes/story/Meaning.cpp \
