2f3a43a6c4aef40f4d3bc6cb262557286394d050
KARIM MIRAZUL <karim@chimple.org>
2017-04-05 14:11:00 +0530

    added AlphaPhonics game

--- goa/Classes/menu/StartMenuScene.h
+++ goa/Classes/menu/StartMenuScene.h
@@ -79,6 +79,7 @@ static const std::string MININGBG = "miningbg";
 static const std::string FARMHOUSE = "farmhouse";
 static const std::string PHONICSFREE = "phonicsfree";
 static const std::string ALPHAARRANGE = "alphaarrange";
+static const std::string ALPHAPHONICS = "aplhaphonics";
 static const std::string CITY1 = "city1";
 static const std::string CITY2 = "city2";
 static const std::string CITY3 = "city3";
