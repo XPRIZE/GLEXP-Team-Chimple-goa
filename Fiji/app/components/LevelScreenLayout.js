e2ea14281429936be72bfceb54a76184b31f6d54
KARIM MIRAZUL <karim@chimple.org>
2017-12-01 13:16:27 +0530

    scrolling done with wrap properties

--- Fiji/app/components/LevelScreenLayout.js
+++ Fiji/app/components/LevelScreenLayout.js
@@ -9,6 +9,21 @@ const TEST_LEVELS = [
     {levelNo: 3, isLock: true, imgPath: '../assets/starImg.png'},
     {levelNo: 4, isLock: true, imgPath: '../assets/starImg.png'},
     {levelNo: 5, isLock: true, imgPath: '../assets/starImg.png'},
+    {levelNo: 6, isLock: false, imgPath: '../assets/starImg.png'},
+    {levelNo: 7, isLock: false, imgPath: '../assets/starImg.png'},
+    {levelNo: 8, isLock: true, imgPath: '../assets/starImg.png'},
+    {levelNo: 9, isLock: true, imgPath: '../assets/starImg.png'},
+    {levelNo: 10, isLock: true, imgPath: '../assets/starImg.png'},
+    {levelNo: 11, isLock: false, imgPath: '../assets/starImg.png'},
+    {levelNo: 12, isLock: false, imgPath: '../assets/starImg.png'},
+    {levelNo: 13, isLock: true, imgPath: '../assets/starImg.png'},
+    {levelNo: 14, isLock: true, imgPath: '../assets/starImg.png'},
+    {levelNo: 15, isLock: true, imgPath: '../assets/starImg.png'},
+    {levelNo: 16, isLock: false, imgPath: '../assets/starImg.png'},
+    {levelNo: 17, isLock: false, imgPath: '../assets/starImg.png'},
+    {levelNo: 18, isLock: true, imgPath: '../assets/starImg.png'},
+    {levelNo: 19, isLock: true, imgPath: '../assets/starImg.png'},
+    {levelNo: 20, isLock: true, imgPath: '../assets/starImg.png'},
 ]
   
 class LevelScreenLayout extends Component {
@@ -26,7 +41,7 @@ class LevelScreenLayout extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <ScrollView>
+                <ScrollView contentContainerStyle={styles.container} scrollEnabled={true}>
                     {this.state.levels.map((element, i) => 
                        <LockUnlock
                        levelNo={element.levelNo}
@@ -44,19 +59,10 @@ class LevelScreenLayout extends Component {
 
 const styles = StyleSheet.create({
     container: {
-        borderWidth: 1,
-        borderRadius: 2,
-        borderColor: '#ddd',
-        borderBottomWidth: 0,
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.1,
-        shadowRadius: 2,
-        elevation: 1,
-        marginLeft: 5,
-        marginRight: 5,
-        marginTop: 10
-    }
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        backgroundColor: '#fff7ee'
+      }
 });
 
 export default LevelScreenLayout;
