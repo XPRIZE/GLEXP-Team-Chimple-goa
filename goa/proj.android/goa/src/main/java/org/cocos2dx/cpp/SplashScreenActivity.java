5f6ff6bd047ded444b204b0d39ca681230cc0994
Ankit Kesharwani <ankitke@maqsoftware.com>
2019-05-23 19:55:29 +0530

    Added permission prompt before the extraction process begins

--- goa/proj.android/goa/src/main/java/org/cocos2dx/cpp/SplashScreenActivity.java
+++ goa/proj.android/goa/src/main/java/org/cocos2dx/cpp/SplashScreenActivity.java
@@ -1,20 +1,27 @@
 package org.cocos2dx.cpp;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Toast;
+
 import com.google.android.vending.expansion.downloader.Helpers;
 import com.maq.xprize.goa.hindi.R;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.zip.ZipFile;
+
 import chimple.DownloadExpansionFile;
 import utils.Zip;
 
@@ -23,55 +30,52 @@ public class SplashScreenActivity extends Activity {
     Intent intent = null;
 
     public static String getUnzippedExpansionFilePath() {
-//        return getContext().getExternalFilesDir(null) + File.separator;
         return "/storage/emulated/0/Android/data/com.maq.xprize.goa.hindi/files/";
     }
 
-    /*@Override
+    @Override
     public void onRequestPermissionsResult(int requestCode,
                                            String[] permissions, int[] grantResults) {
         switch (requestCode) {
-            case 99: {
+            case 1: {
                 // If request is cancelled, the result arrays are empty.
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    System.out.println("Permission granted");
                     new DownloadFile().execute(null, null, null);
                 } else {
-                    System.out.println("Permission not granted");
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-                    onDestroy();
+                    Toast.makeText(this, "Permission required!", Toast.LENGTH_LONG).show();
+                    finish();
                 }
             }
-
-            // other 'case' lines to check for other
-            // permissions this app might request.
         }
-    }*/
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       /* if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                != PackageManager.PERMISSION_GRANTED &&
-                ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
-                        != PackageManager.PERMISSION_GRANTED) {*/
-        // Permission is not granted
-        /*}*/
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        if (Build.VERSION.SDK_INT > 11 && Build.VERSION.SDK_INT < 19) { // lower api
+        if (Build.VERSION.SDK_INT < 19) { // lower api
             View v = this.getWindow().getDecorView();
             v.setSystemUiVisibility(View.GONE);
-        } else if (Build.VERSION.SDK_INT >= 19) {
+        } else {
             //for new api versions.
             View decorView = this.getWindow().getDecorView();
             int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
             decorView.setSystemUiVisibility(uiOptions);
         }
         setContentView(R.layout.activity_splash_screen);
-        new DownloadFile().execute(null, null, null);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        } else {
+            new DownloadFile().execute(null, null, null);
+        }
     }
 
     /* function to call the main application after extraction */
@@ -84,7 +88,6 @@ public class SplashScreenActivity extends Activity {
     public void unzipFile() {
         try {
             String filePath = getExpansionFilePath();
-//            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
             File file = new File(filePath);
             ZipFile zipFile = new ZipFile(file);
             Zip _zip = new Zip(zipFile, this);
@@ -98,8 +101,7 @@ public class SplashScreenActivity extends Activity {
             _zip.close();
             toCallApplication();
         } catch (IOException ie) {
-            System.out.println("Unzip exception\t" + ie);
-            unzipFile();
+            System.out.println(ie);
         }
         return;
     }
